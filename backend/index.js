3bf5dc46c8c36b489f9eef8e0a0ef37880bdc46f
Harsh Dhanawade <harshd2911@gmail.com>
2026-06-03 10:34:45 +0530

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const http = require("http");
+// const { Server } = require("socket.io");
+// const mainRouter = require("./routes/main.router");
+
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const { initRepo } = require("./controllers/init");
+const { addRepo } = require("./controllers/add");
+const { commitRepo } = require("./controllers/commit");
+const { pushRepo } = require("./controllers/push");
+const { pullRepo } = require("./controllers/pull");
+const { revertRepo } = require("./controllers/revert");
+
+dotenv.config();
+
+yargs(hideBin(process.argv))
+    .command("start", "Starts a new server", {}, initRepo)
+    .command("init", "Initialise a new repository", {}, initRepo)
+    .command(
+        "add <file>", 
+        "Add a file to the repository", 
+        (yargs) => {
+            yargs.positional("file", {
+                describe: "File to add to the staging area",
+                type: "string",
+            });
+        }, 
+        (argv) => {
+            addRepo(argv.file);
+        }
+    )
+    .command(
+    "commit <message>",
+    "Commit the staged files",
+    (yargs) => {
+      yargs.positional("message", {
+        describe: "Commit message",
+        type: "string",
+      });
+    },
+    (argv) => {
+      commitRepo(argv.message);
+    }
+    )
+    .command("push", "Push commits to S3", {}, pushRepo)
+    .command("pull", "Pull commits from S3", {}, pullRepo)
+    .command(
+        "revert <commitID>",
+        "Revert to a specific commit",
+        (yargs) => {
+        yargs.positional("commitID", {
+            describe: "Comit ID to revert to",
+            type: "string",
+        });
+        },
+        (argv) => {
+            revertRepo(argv.commitID);
+        }
+    )
+    .demandCommand(1, "You need at least one command").
+    help().argv;
+
+function startServer(){
+    const app = express();
+    const port = process.env.PORT || 3000;
+
+    app.use(bodyParser.json());
+    app.use(express.json());
+
+    const mongoURI = process.env.MONGODB_URI;
+
+    mongoose.connect(mongoURI).
+    then(() => console.log("Mongoos connected!")).
+    catch((err) => console.error("Unable to connect: ", err));
+}
