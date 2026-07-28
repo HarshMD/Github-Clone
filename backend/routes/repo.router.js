eb493af2c5dd41203c36929b589acd9931a8e5db
Harsh Dhanawade <harshd2911@gmail.com>
2026-07-28 14:36:53 +0530

    Added Repo Controller functionality

--- backend/routes/repo.router.js
+++ backend/routes/repo.router.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const repoController = require("../controllers/repoController");
 
 const repoRouter = express.Router();
@@ -6,8 +6,8 @@ const repoRouter = express.Router();
 repoRouter.post("/repo/create", repoController.createRepository);
 repoRouter.get("/repo/all", repoController.getAllRepositories);
 repoRouter.get("/repo/:id", repoController.fetchRepositoryById);
-repoRouter.get("/repo/:name", repoController.fetchRepositoryByName);
-repoRouter.get("/repo/:userID", repoController.fetchRepositoryForCurrentUser);
+repoRouter.get("/repo/name/:name", repoController.fetchRepositoryByName);
+repoRouter.get("/repo/user/:userID", repoController.fetchRepositoriesForCurrentUser);
 repoRouter.put("/repo/update/:id", repoController.updateRepositoryById);
 repoRouter.delete("/repo/delete/:id", repoController.deleteRepositoryById);
 repoRouter.patch("/repo/toggle/:id", repoController.toggleVisibilityById);
